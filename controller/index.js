87b65548d525163342aef1b26c2bf36a996747db
doducbinh <doducbinh1995@gmail.com>
2020-06-08 22:52:39 +0700

    update

--- controller/index.js
+++ controller/index.js
@@ -1,16 +1,19 @@
-var userName = "Đỗ Đức Bình";
-const PRIVATEPERSONALCHANEL = "private-chanel";
+var userName = "";
+const PRIVATEPERSONALCHANEL = "join-success";
+const GETNEWSMESSAGE = "get-new-message";
+const SENDINGMESSAGESUCESS = "sending-success";
+
 module.exports = async function socketIoControl(socket, io) {
     console.log('a user connected');
     socket.emit("wellCome", { message: "Chào anh em" })
     socket.on("join-chat-rom", data => {
+        userName = data.userName;
         console.log(data)
-        console.log("user" + userName + " join chat rom");
-        socket.emit("join-chat-rom" + userName, { message: "success", status: true })
+        socket.emit(PRIVATEPERSONALCHANEL, { status: true })
     })
     socket.on('send-message', (data) => {
         console.log("recived message")
-        socket.emit(PRIVATEPERSONALCHANEL,{type:"message has send", data: { id: data.id}})
-        socket.broadcast.emit("get-message",{sender: userName, message: data.message, id: data.id})
+        socket.emit(PRIVATEPERSONALCHANEL, { id: data.id })
+        socket.broadcast.emit(GETNEWSMESSAGE, { sender: userName, message: data.message, id: data.id })
     })
 }
